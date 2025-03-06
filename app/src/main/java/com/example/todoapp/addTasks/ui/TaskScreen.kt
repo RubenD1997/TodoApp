c8847bb863bf29891cdbcdf29d08bebf5cd124cc
RubenD1997 <ruben28-1997@hotmail.com>
2025-03-06 13:45:18 -0500

    Add dialog Tasks

--- app/src/main/java/com/example/todoapp/addTasks/ui/TaskScreen.kt
+++ app/src/main/java/com/example/todoapp/addTasks/ui/TaskScreen.kt
@@ -1,9 +1,97 @@
 package com.example.todoapp.addTasks.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun TasksScreen(modifier: Modifier, tasksViewModel: TasksViewModel) {
+    var showDialog by rememberSaveable { mutableStateOf(false) }
+    Box(modifier = modifier.fillMaxSize()) {
+        AddTaskDialog(showDialog) { _ ->
+            showDialog = false
+        }
+        FabDialog(modifier = Modifier.align(Alignment.BottomEnd)) {
+            showDialog = true
+        }
+    }
+}
 
+@Composable
+fun FabDialog(modifier: Modifier, openDialog: () -> Unit) {
+    FloatingActionButton(
+        onClick = { openDialog() },
+        modifier = modifier.padding(16.dp)
+    ) {
+        Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTaskDialog(show: Boolean = false, onTaskAdded: (String) -> Unit) {
+    var myTask by rememberSaveable { mutableStateOf("") }
+    if (show) {
+        BasicAlertDialog(onDismissRequest = { }) {
+            Card(
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surface
+                )
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Añade tu tarea",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Spacer(modifier = Modifier.size(16.dp))
+                    TextField(
+                        value = myTask,
+                        onValueChange = { myTask = it },
+                        singleLine = true,
+                        maxLines = 1,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.size(16.dp))
+                    Button(onClick = {
+                        onTaskAdded(myTask)
+                    }, modifier = Modifier.fillMaxWidth()) {
+                        Text(text = "Añadir tarea")
+                    }
+                }
+            }
+        }
+    }
 }
