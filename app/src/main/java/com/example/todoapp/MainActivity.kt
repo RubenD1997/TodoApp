e139bc9f1d817d682807cbdfcc320ec7b5659277
RubenD1997 <ruben28-1997@hotmail.com>
2025-03-06 12:27:12 -0500

    Upload project

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.todoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.todoapp.ui.theme.TodoAppTheme
+import com.example.todoapp.addTasks.ui.TasksScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TodoAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TasksScreen(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
