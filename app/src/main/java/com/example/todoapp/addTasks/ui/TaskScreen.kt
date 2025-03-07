32c88b44f4347fc84981db76b1287e13bb9e70a4
RubenD1997 <ruben28-1997@hotmail.com>
2025-03-07 14:27:31 -0500

    Add create/delete tasks

--- app/src/main/java/com/example/todoapp/addTasks/ui/TaskScreen.kt
+++ app/src/main/java/com/example/todoapp/addTasks/ui/TaskScreen.kt
@@ -1,12 +1,16 @@
 package com.example.todoapp.addTasks.ui
 
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -14,6 +18,7 @@ import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -23,12 +28,14 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.input.pointer.pointerInput
+import com.example.todoapp.addTasks.ui.model.TasksModel
 
 @Composable
 fun TasksScreen(modifier: Modifier, tasksViewModel: TasksViewModel) {
@@ -43,6 +50,48 @@ fun TasksScreen(modifier: Modifier, tasksViewModel: TasksViewModel) {
         }
         FabDialog(modifier = Modifier.align(Alignment.BottomEnd)) {
             tasksViewModel.onDialogShow()
+        }
+        TasksList(tasksViewModel)
+    }
+}
+
+@Composable
+fun TasksList(tasksViewModel: TasksViewModel) {
+    val myTasks: List<TasksModel> = tasksViewModel.tasks
+    LazyColumn {
+        items(myTasks, key = { it.id }) {
+            ItemTask(it, tasksViewModel)
+        }
+    }
+}
+
+@Composable
+fun ItemTask(tasksModel: TasksModel, tasksViewModel: TasksViewModel) {
+    Card(
+        shape = RoundedCornerShape(15.dp),
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .fillMaxWidth()
+            .pointerInput(Unit) {
+                detectTapGestures(onLongPress = {
+                    tasksViewModel.onItemRemove(tasksModel)
+                })
+            },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = tasksModel.task, modifier = Modifier.weight(1f))
+            Checkbox(checked = tasksModel.selected, onCheckedChange = {
+                tasksViewModel.onCheckBoxSelected(tasksModel)
+            })
         }
     }
 }
@@ -97,6 +146,7 @@ fun AddTaskDialog(show: Boolean = false, onDialogAction: (ActionDialog) -> Unit)
                     Spacer(modifier = Modifier.size(16.dp))
                     Button(onClick = {
                         onDialogAction(ActionDialog.AddTask(myTask))
+                        myTask = ""
                     }, modifier = Modifier.fillMaxWidth()) {
                         Text(text = "Añadir tarea")
                     }
