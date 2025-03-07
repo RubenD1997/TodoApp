0b7934cd2600e24135bdc1d4fea6b988fc13413b
RubenD1997 <ruben28-1997@hotmail.com>
2025-03-07 10:15:45 -0500

    Finish dialog tasks

--- app/src/main/java/com/example/todoapp/addTasks/ui/TasksViewModel.kt
+++ app/src/main/java/com/example/todoapp/addTasks/ui/TasksViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.todoapp.addTasks.ui
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -7,4 +10,19 @@ import javax.inject.Inject
 @HiltViewModel
 class TasksViewModel @Inject constructor() : ViewModel() {
 
+    private val _showDialog = MutableLiveData<Boolean>()
+    var showDialog: LiveData<Boolean> = _showDialog
+
+    fun onDialogClose() {
+        _showDialog.value = false
+    }
+
+    fun onTaskCreate(task: String) {
+        Log.i("TAG", "onTaskCreate: $task")
+        onDialogClose()
+    }
+
+    fun onDialogShow() {
+        _showDialog.value = true
+    }
 }
